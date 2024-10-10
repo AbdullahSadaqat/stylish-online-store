f33cfe849a5f4609a5a110c92de9e9fd19fc2062
Abdullah Sadaqat <158241075+AbdullahSadaqat@users.noreply.github.com>
2024-10-10 23:07:00 +0500

    Update Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-16">
           <div className='flex-shrink-0 flex items-center'>
-            <Link to="/" className="text-3xl font-bold text-gray-900 flex-shrink-0 flex items-center">Stylish <span className="text-xs ml-2 text-gray-500">Online Store</span></Link>
+            <Link to="/" className="text-3xl font-bold text-gray-900 flex-shrink-0 flex items-center">Stylish <span className="text-xs ml-2 text-gray-500 hidden lg:inline">Online Store</span></Link>
           </div>
 
           <div className='flex items-center gap-7'>

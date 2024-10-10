888bbfa8886d4b8e312879bf61f7a99cfc7fa3b9
Abdullah Sadaqat <158241075+AbdullahSadaqat@users.noreply.github.com>
2024-10-10 23:13:31 +0500

    Update ShippingInfo

--- src/components/ShippingInfo.jsx
+++ src/components/ShippingInfo.jsx
@@ -23,11 +23,11 @@ const ShippingInfo = ({ onSubmit }) => {
     });
 
     return (
-        <div className='flex '>
-            <div className='lg:w-1/2 flex items-center'>
+        <div className='flex flex-wrap'>
+            <div className='w-full lg:w-1/2 flex items-center'>
                 <img src="/images/delivery.png" className='w-full' alt="delivery images" />
             </div>
-            <div className='lg:w-1/2'>
+            <div className='w-full lg:w-1/2'>
 
                 <form onSubmit={formik.handleSubmit} className="bg-white p-6 lg:w-4/5 mx-auto">
                     <h2 className="text-2xl mb-4">Shipping Information</h2>
